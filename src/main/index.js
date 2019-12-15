d31ce798015600d9eddc69ece25856ce44ba035e
Aveza <mikkottis@hotmail.com>
2019-12-15 04:21:00 +0200

    Fixed electron build errors. Cleanup

--- src/main/index.js
+++ src/main/index.js
@@ -18,7 +18,7 @@ function createWindow() {
   mainWindow.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`,
+      : `file://${path.join(__dirname, '../../build/index.html')}`,
   )
 
   mainWindow.on('closed', () => {
